4afbe850ef50875ef77b4990291bb2ac33a7ec99
wug <768986235@qq.com>
2024-05-20 23:46:55 +0800

    第四次提交

--- src/main/java/com/atguigu/controller/HelloController.java
+++ src/main/java/com/atguigu/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
     @ResponseBody
     @RequestMapping("/hello")
-    public String hello() {
+    public String hello(){
         return "Hello World!";
     }
 }
